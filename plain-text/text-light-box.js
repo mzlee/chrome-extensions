daa61f4a63e8e292edf1755ce1bf6403fd25875a
Michael Lee <mzlee@cs.utexas.edu>
2011-04-13 18:16:33 -0500

    Added a very basic text viewer.  It's pretty bad...

---
+++ plain-text/text-light-box.js
@@ -0,0 +1,9 @@
+tlb = document.createElement('div');
+tlb.id = 'textLightBox';
+tlb.background = "#fff";
+tlb.style.display = 'none';
+p = document.createElement('pre');
+p.id = 'preId';
+tlb.appendChild(p);
+
+document.body.appendChild(tlb);
