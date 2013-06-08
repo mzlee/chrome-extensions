52affc6f9ebe7a25e8ae7a6969e5f03dfb028700
Michael Lee <mzlee@cs.utexas.edu>
2013-06-08 17:51:20 -0400

    A replacement for bookmarks.
    The main thing this adds is a tempory bookmark -- it takes a snapshot of the visible page.

---
+++ bookmarks/lib.js
@@ -0,0 +1,34 @@
+function hashCode(str) {
+    var hash = 0, i, char;
+    if (typeof str === "string") {
+        if (str.length == 0) return hash;
+        for (i = 0, l = str.length; i < l; i++) {
+            char = str.charCodeAt(i);
+            hash = ((hash << 5) - hash) + char;
+            hash |= 0;
+        }
+    } else {
+        hash = str | 0;
+    }
+    console.log("Hashed " + str + " to " + hash);
+    return hash;
+}
+
+function getLocalData(objName) {
+    var hc = hashCode(objName);
+    console.log("Retrieving " + objName + " -> " + hc);
+    if (localStorage[hc] != undefined) {
+        return JSON.parse(localStorage[hc]);
+    }
+    return null;
+}
+
+function setLocalData(objName, data) {
+    var hc = hashCode(objName);
+    console.log("Storing " + objName + " " + hc);
+    localStorage[hc] = JSON.stringify(data);
+}
+
+function deleteLocalData(objName) {
+    delete(localStorage[hashCode(objName)]);
+}
