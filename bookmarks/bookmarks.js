c1c2abc0f44ef49e92836970e1aaac49392d41ed
Michael Lee <mzlee@cs.utexas.edu>
2013-06-08 21:27:37 -0500

    Moving around the short-term bookmarks

--- bookmarks/bookmarks.js
+++ bookmarks/bookmarks.js
@@ -20,7 +20,9 @@ function dumpTreeNodes(bookmarkNodes, query) {
   var list = $('<ul>');
   var i;
   for (i = 0; i < bookmarkNodes.length; i++) {
-    list.append(dumpNode(bookmarkNodes[i], query));
+    var node = dumpNode(bookmarkNodes[i], query);
+    if (node)
+        list.append(node);
   }
   return list;
 }
@@ -33,36 +35,56 @@ function dumpNode(bookmarkNode, query) {
     }
     var anchor = $('<a>');
     anchor.attr('href', bookmarkNode.url);
-    anchor.text(bookmarkNode.title);
     var img = getLocalData(bookmarkNode.url);
-    if (img !== null) {
+    if (img != null) {
         var image = $('<img>');
         image.attr("src",  img);
         image.attr("width", "360");
         image.attr("height", "200");
-        image.attr("style", "border: solid black 3; padding: 5px; background: #999999;");
-        anchor.append($("<br />"));
+        image.attr("style", "border: solid black 5; padding: 5px; margin: 5px; background: #999999;");
+        anchor.attr("width", "360");
+        anchor.attr("height", "200");
         anchor.append(image);
+    } else {
+        anchor.text(bookmarkNode.title);
     }
     /*
      * When clicking on a bookmark in the extension, a new tab is fired with
      * the bookmark url.
      */
-    anchor.click(function() {
+    anchor.click(function(e) {
       chrome.tabs.create({url: bookmarkNode.url});
+      e.preventDefault();
     });
-    var span = $('<span>');
-    var options = bookmarkNode.children ?
-      $('<span>[<a href="#" id="addlink">Add</a>]</span>') :
-      $('<span>[<a id="editlink" href="#">Edit</a> <a id="deletelink" ' +
-        'href="#">Delete</a>]</span>');
+    var span;
+    if (img != null) {
+        span = $('<div class="float">');
+    } else {
+        span = $('<span>');
+    }
+    var options;
+    if (img != null) {
+        var overlay = $('<span class="overlay">');
+        overlay.html('<div class="spacer"></div>[' +
+                     '<a id="editlink" href="#">Edit</a>&nbsp;&nbsp;&nbsp;&nbsp;' +
+                     '<a id="deletelink" href="#">Delete</a>]');
+        overlay.click(function() {
+                chrome.tabs.create({url: bookmarkNode.url});
+            });
+        options = overlay;
+    } else {
+        options = bookmarkNode.children ?
+            $('<span>[<a href="#" id="addlink">Add</a>]</span>') :
+            $('<span>[<a id="editlink" href="#">Edit</a> <a id="deletelink" ' +
+              'href="#">Delete</a>]</span>');
+    }
     var edit = bookmarkNode.children ? $('<table><tr><td>Name</td><td>' +
-      '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' +
-      '</td></tr></table>') : $('<input>');
+                                         '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' +
+                                         '</td></tr></table>') : $('<input>');
     // Show add and edit links when hover over.
-        span.hover(function() {
+    span.hover(function() {
         span.append(options);
-        $('#deletelink').click(function() {
+        $('#deletelink').click(function(e) {
           $('#deletedialog').empty().dialog({
                  autoOpen: false,
                  title: 'Confirm Deletion',
@@ -76,6 +98,7 @@ function dumpNode(bookmarkNode, query) {
                  buttons: {
                    'Yes, Delete It!': function() {
                       chrome.bookmarks.remove(String(bookmarkNode.id));
+                      deleteLocalData(bookmarkNode.url);
                       span.parent().remove();
                       $(this).dialog('destroy');
                     },
@@ -84,6 +107,7 @@ function dumpNode(bookmarkNode, query) {
                     }
                  }
                }).dialog('open');
+          e.preventDefault();
          });
         $('#addlink').click(function() {
           $('#adddialog').empty().append(edit).dialog({autoOpen: false,
@@ -101,7 +125,7 @@ function dumpNode(bookmarkNode, query) {
             }
           }}).dialog('open');
         });
-        $('#editlink').click(function() {
+        $('#editlink').click(function(e) {
          edit.val(anchor.text());
          $('#editdialog').empty().append(edit).dialog({autoOpen: false,
            closeOnEscape: true, title: 'Edit Title', modal: true,
@@ -118,6 +142,7 @@ function dumpNode(bookmarkNode, query) {
                  $(this).dialog('destroy');
              }
          }}).dialog('open');
+         e.preventDefault();
         });
         options.fadeIn();
       },
@@ -129,6 +154,10 @@ function dumpNode(bookmarkNode, query) {
   var li = $(bookmarkNode.title ? '<li>' : '<div>').append(span);
   if (bookmarkNode.children && bookmarkNode.children.length > 0) {
     li.append(dumpTreeNodes(bookmarkNode.children, query));
+  }
+  if (img != null) {
+      $('#short').append(span);
+      return false;
   }
   return li;
 }
