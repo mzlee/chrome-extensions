4f0df5e48f7b0d1a98abd61da92b7a62264474fd
mzlee <mzlee@cs.utexas.edu>
2013-08-04 03:40:22 -0500

    [short-bookmark] Added image resizing so we don't run out of space so quick

--- bookmarks/short-bookmark.js
+++ bookmarks/short-bookmark.js
@@ -2,7 +2,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         var name = tab.title;
         var url  = tab.url;
         chrome.tabs.captureVisibleTab(null, {"format":"png"}, function (image) {
-                setLocalData(""+url, image);
-                chrome.bookmarks.create({parentId: "3", title: name, url: url});
+	        var canvas = document.getElementById('scratch_pad');
+	        var context = canvas.getContext('2d');
+	        var img = new Image;
+	        img.onload = function () {
+		    context.drawImage(img, 0, 0, img.width, img.height,
+				      0, 0, 360, 200);
+		    setLocalData(""+url, canvas.toDataURL());
+                    chrome.bookmarks.create({parentId: "3", title: name, url: url});
+		}
+	        img.src = image;
         });
 });
